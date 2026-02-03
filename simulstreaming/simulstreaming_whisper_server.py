0a4d3914febb9a9ef1dce623646a11f5fe676a3a
Hrólfur <33896457+hrolfurgylfa@users.noreply.github.com>
2026-02-03 22:42:58 +0000

    Make simulstreaming and simuilstreaming_server executables

--- simulstreaming/simulstreaming_whisper_server.py
+++ simulstreaming/simulstreaming_whisper_server.py
@@ -2,5 +2,8 @@
 from simulstreaming.simulstreaming_whisper import simulwhisper_args, simul_asr_factory
 from simulstreaming.whisper_streaming.whisper_server import main_server
 
-if __name__ == "__main__":
+def main():
     main_server(simul_asr_factory, add_args=simulwhisper_args)
+
+if __name__ == "__main__":
+    main()
